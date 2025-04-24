71cdd8c924973320956d309adeb6691079d77940
Minh Pham <pcminh0505@gmail.com>
2025-04-24 19:16:00 +0700

    chore: Clean up

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -4,17 +4,10 @@ import { DataTable } from "@/components/data-table";
 import { SectionCards } from "@/components/section-cards";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import { redirect } from "next/navigation";
-import { createClient } from "@/utils/supabase/server";
 
 import MOCK_DATA from "./data.json";
 
-export default async function Page() {
-  const supabase = await createClient();
-  const { data, error } = await supabase.auth.getUser();
-  if (error || !data?.user) {
-    redirect("/login");
-  }
+export default function Page() {
   return (
     <SidebarProvider
       style={
